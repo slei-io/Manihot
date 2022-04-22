c5bc96f5d34a24ec01ccad6b244c13bf6f1334ed
Olúwatìmìlé̩hìn Ako̩gun <hello@timiakogun.com>
2022-04-22 19:09:41 +0100

    feat: Add base serializer class

---
+++ app/core/serializer.py
@@ -0,0 +1,10 @@
+from mimetypes import init
+from msilib.schema import Class
+
+
+class Serializer():
+    def __init__(self, data):
+        self.data
+
+    def get_data(self):
+        return self.data
